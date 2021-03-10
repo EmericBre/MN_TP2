e80ac0062f784366dc7768d7a680a63e18d6d8e0
Eve Poitevin <poitevineve@gmail.com>
2021-03-10 17:51:23 +0100

    Blas 1 Blas 2 Blas 3 fait sans tests

---
+++ TPBLAS/src/gemm.c
@@ -0,0 +1,94 @@
+#include "mnblas.h"
+#include <stdio.h>
+#include "complexe.h"
+#include "math.h"
+
+#define max(a, b) ((a) > (b) ? (a) : (b))
+
+void mncblas_sgemm(MNCBLAS_LAYOUT layout, MNCBLAS_TRANSPOSE TransA,
+                 MNCBLAS_TRANSPOSE TransB, const int M, const int N,
+                 const int K, const float alpha, const float *A,
+                 const int lda, const float *B, const int ldb,
+                 const float beta, float *C, const int ldc)
+{
+   register unsigned int i;
+   register unsigned int j;
+   register unsigned int k;
+   register float save = 0.0;
+
+   for (i = 0; i < M; i++) {
+      for (j = 0; j < N; j++) {
+         for (k = 0; k < K; k++) {
+            save += alpha*A[i*K+k]*B[k*N+j]; 
+         }
+         save += beta*C[i*N+j];
+         C[i*N+j] = save;
+      }
+   }
+}
+
+void mncblas_dgemm(MNCBLAS_LAYOUT layout, MNCBLAS_TRANSPOSE TransA,
+                 MNCBLAS_TRANSPOSE TransB, const int M, const int N,
+                 const int K, const double alpha, const double *A,
+                 const int lda, const double *B, const int ldb,
+                 const double beta, double *C, const int ldc)
+{
+   register unsigned int i;
+   register unsigned int j;
+   register unsigned int k;
+   register double save = 0.0;
+
+   for (i = 0; i < M; i++) {
+      for (j = 0; j < N; j++) {
+         for (k = 0; k < K; k++) {
+            save += alpha*A[i*K+k]*B[k*N+j]; 
+         }
+         save += beta*C[i*N+j];
+         C[i*N+j] = save;
+      }
+   }
+}
+
+void mncblas_cgemm(MNCBLAS_LAYOUT layout, MNCBLAS_TRANSPOSE TransA,
+                 MNCBLAS_TRANSPOSE TransB, const int M, const int N,
+                 const int K, const void *alpha, const void *A,
+                 const int lda, const void *B, const int ldb,
+                 const void *beta, void *C, const int ldc)
+{
+   register unsigned int i;
+   register unsigned int j;
+   register unsigned int k;
+   register complexe_float_t save;
+
+   for (i = 0; i < M; i++) {
+      for (j = 0; j < N; j++) {
+         for (k = 0; k < K; k++) {
+            save = add_complexe_float(save, mult_complexe_float(mult_complexe_float(*(complexe_float_t*)alpha, ((complexe_float_t*)A)[i*K+k]), ((complexe_float_t*)B)[k*N+j])); 
+         }
+         save = add_complexe_float(save, mult_complexe_float(*(complexe_float_t*)beta, ((complexe_float_t*)C)[i*N+j]));
+         ((complexe_float_t*)C)[i*N+j] = save;
+      }
+   }
+}
+
+void mncblas_zgemm(MNCBLAS_LAYOUT layout, MNCBLAS_TRANSPOSE TransA,
+                 MNCBLAS_TRANSPOSE TransB, const int M, const int N,
+                 const int K, const void *alpha, const void *A,
+                 const int lda, const void *B, const int ldb,
+                 const void *beta, void *C, const int ldc)
+{
+   register unsigned int i;
+   register unsigned int j;
+   register unsigned int k;
+   register complexe_double_t save;
+
+   for (i = 0; i < M; i++) {
+      for (j = 0; j < N; j++) {
+         for (k = 0; k < K; k++) {
+            save = add_complexe_double(save, mult_complexe_double(mult_complexe_double(*(complexe_double_t*)alpha, ((complexe_double_t*)A)[i*K+k]), ((complexe_double_t*)B)[k*N+j])); 
+         }
+         save = add_complexe_double(save, mult_complexe_double(*(complexe_double_t*)beta, ((complexe_double_t*)C)[i*N+j]));
+         ((complexe_double_t*)C)[i*N+j] = save;
+      }
+   }
+}
--- TPBLAS/src/gemv.c
+++ TPBLAS/src/gemv.c
@@ -3,30 +3,81 @@
 #include "complexe.h"
 #include "math.h"
 
+#define max(a, b) ((a) > (b) ? (a) : (b))
+
 void mncblas_sgemv(const MNCBLAS_LAYOUT layout,
-                 const MNCBLAS_TRANSPOSE TransA, const int M, const int N,
-                 const float alpha, const float *A, const int lda,
-                 const float *X, const int incX, const float beta,
-                 float *Y, const int incY)
-                 {
-                    register unsigned int i = 0;
-                    register unsigned int j = 0;
-                 }
+                   const MNCBLAS_TRANSPOSE TransA, const int M, const int N,
+                   const float alpha, const float *A, const int lda,
+                   const float *X, const int incX, const float beta,
+                   float *Y, const int incY)
+{
+   register unsigned int i;
+   register unsigned int j;
+   register float save;
+
+   for (i = 0; i < M; i += incX) {
+      save = beta*Y[i];
+      for (j = 0; j < N; j += incY) {
+         save += alpha*A[i*N+j]*X[j]; 
+      }
+      Y[i] = save;
+   }
+
+}
 
 void mncblas_dgemv(MNCBLAS_LAYOUT layout,
-                 MNCBLAS_TRANSPOSE TransA, const int M, const int N,
-                 const double alpha, const double *A, const int lda,
-                 const double *X, const int incX, const double beta,
-                 double *Y, const int incY);
+                   MNCBLAS_TRANSPOSE TransA, const int M, const int N,
+                   const double alpha, const double *A, const int lda,
+                   const double *X, const int incX, const double beta,
+                   double *Y, const int incY)
+{
+   register unsigned int i;
+   register unsigned int j;
+   register double save;
+
+   for (i = 0; i < M; i += incX) {
+      save = beta*Y[i];
+      for (j = 0; j < N; j += incY) {
+         save += alpha*A[i*N+j]*X[j]; 
+      }
+      Y[i] = save;
+   }
+}
 
 void mncblas_cgemv(MNCBLAS_LAYOUT layout,
-                 MNCBLAS_TRANSPOSE TransA, const int M, const int N,
-                 const void *alpha, const void *A, const int lda,
-                 const void *X, const int incX, const void *beta,
-                 void *Y, const int incY);
+                   MNCBLAS_TRANSPOSE TransA, const int M, const int N,
+                   const void *alpha, const void *A, const int lda,
+                   const void *X, const int incX, const void *beta,
+                   void *Y, const int incY)
+{
+   register unsigned int i;
+   register unsigned int j;
+   register complexe_float_t save ;
+
+   for (i = 0; i < M; i += incX) {
+      save = mult_complexe_float(*(complexe_float_t*)beta, ((complexe_float_t*)Y)[i]);
+      for (j = 0; j < N; j += incY) {
+         save = add_complexe_float(save, mult_complexe_float(mult_complexe_float(*(complexe_float_t*)alpha, ((complexe_float_t*)A)[i*N+j]),((complexe_float_t*)X)[j]));
+      }
+      ((complexe_float_t*)Y)[i] = save;
+   }
+}
 
 void mncblas_zgemv(MNCBLAS_LAYOUT layout,
-                 MNCBLAS_TRANSPOSE TransA, const int M, const int N,
-                 const void *alpha, const void *A, const int lda,
-                 const void *X, const int incX, const void *beta,
-                 void *Y, const int incY);
+                   MNCBLAS_TRANSPOSE TransA, const int M, const int N,
+                   const void *alpha, const void *A, const int lda,
+                   const void *X, const int incX, const void *beta,
+                   void *Y, const int incY)
+{
+   register unsigned int i;
+   register unsigned int j;
+   register complexe_double_t save;
+
+   for (i = 0; i < M; i += incX) {
+      save = mult_complexe_double(*(complexe_double_t*)beta, ((complexe_double_t*)Y)[i]);
+      for (j = 0; j < N; j += incY) {
+         save = add_complexe_double(save, mult_complexe_double(mult_complexe_double(*(complexe_double_t*)alpha, ((complexe_double_t*)A)[i*N+j]),((complexe_double_t*)X)[j]));
+      }
+      ((complexe_double_t*)Y)[i] = save;
+   }
+}
